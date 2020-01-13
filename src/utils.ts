751a84646d5e69d4f2a33b2c3840c10c6b803f0a
2a5f <aditrc@outlook.com>
2020-01-13 20:31:30 +0800

    queue tokenizer

--- src/utils.ts
+++ src/utils.ts
@@ -7,3 +7,23 @@ export function isVoid(v: any): v is void {
 export function getErrorsMsgs(errors: Errors[]) {
     return errors.flatMap(e => [`${e.msg}`, `    at ${e.range.from.line + 1}:${e.range.from.char + 1} to ${e.range.to.line + 1}:${e.range.to.char + 1}`])
 }
+
+export function showErr(errmsg: string[]): never {
+    throw new SyntaxError(`\n    ${errmsg.join('\n    ')}\n`)
+}
+
+export function next_micro_tick(): Promise<void> {
+    return new Promise(res => res())
+}
+
+export function next_macro_tick(): Promise<void> {
+    return new Promise(res => {
+        setTimeout(res)
+    })
+}
+
+export function delay(timeout?: number): Promise<void> {
+    return new Promise(res => {
+        setTimeout(res, timeout)
+    })
+}
