98363eb63bd74d3093cc0aff0c7c114ebb7291eb
2a5f <aditrc@outlook.com>
2020-01-14 19:10:48 +0800

    alapha

---
+++ rollup.config.es.js
@@ -0,0 +1,11 @@
+export default {
+    input: './dist/es/index.js',
+    output: {
+        file: './dist/cbon.mjs',
+        format: 'esm',
+        name: 'cbon',
+    },
+    plugins: [
+        
+    ]
+};
---
+++ rollup.config.js
@@ -0,0 +1,11 @@
+export default {
+    input: './dist/es/index.js',
+    output: {
+        file: './dist/cbon.js',
+        format: 'umd',
+        name: 'cbon',
+    },
+    plugins: [
+        
+    ]
+};
